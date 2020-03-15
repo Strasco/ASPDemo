6ebbc38d800332512c037af7b743726af199e3ab
Vlad Zaharia <vlad.zaharia95@gmail.com>
2020-03-15 18:51:17 +0100

    Fixed untracked files

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,31 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age;
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Corey", Age = 38 });
+            people.Add(new PersonModel { FirstName = "Vlad", LastName = "Zaharia", Age = 3 });
+            people.Add(new PersonModel { FirstName = "Alex", LastName = "BewBew", Age = 45 });
+
+            return View(people);
+        }
+    }
+}
